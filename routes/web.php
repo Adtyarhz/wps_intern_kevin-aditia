0ae6eb1284552acf4a6fdfeb6cc6ff192b296de3
adtyarhz <149128711+Adtyarhz@users.noreply.github.com>
2025-04-22 19:31:23 +0700

    Make middleware

--- routes/web.php
+++ routes/web.php
@@ -2,16 +2,31 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
     'verified',
 ])->group(function () {
+    
     Route::get('/dashboard', function () {
         return view('dashboard');
     })->name('dashboard');
+
+    Route::middleware('role:direktur')->group(function () {
+        Route::get('/direktur', function () {
+            return view('direktur.dashboard');
+        })->name('direktur.dashboard');
+    });
+
+    Route::middleware('role:manager')->group(function () {
+        Route::get('/manager', function () {
+            return view('manager.dashboard');
+        })->name('manager.dashboard');
+    });
+
+    Route::middleware('role:staff')->group(function () {
+        Route::get('/staff', function () {
+            return view('staff.dashboard');
+        })->name('staff.dashboard');
+    });
 });
